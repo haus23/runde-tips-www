bdb8f68a41818f584f2b28c75a1d07a2b92bb5da
Micha Buchholz <micha@haus23.net>
2023-11-28 08:27:42 +0100

    Create initial remix app with vite toolchain.

---
+++ app/routes/_index.jsx
@@ -0,0 +1,14 @@
+export const meta = () => {
+  return [
+    { title: "runde.tips" },
+    { name: "description", content: "Haus23 Fussball Tipprunde" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>runde.tips</h1>
+    </div>
+  );
+}
