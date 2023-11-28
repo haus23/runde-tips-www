6959edb2ec6acc3d028f0d0688e9d80a1f09e11d
Micha Buchholz <micha@haus23.net>
2023-11-28 09:49:09 +0100

    Format files.

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [remix()],
+	plugins: [remix()],
 });
