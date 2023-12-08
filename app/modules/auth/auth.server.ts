5be0220336f93cd0fd32d371c84f16945dcc2a52
Micha Buchholz <micha@haus23.net>
2023-12-08 20:57:13 +0100

    fix: Redirect to referer. No need to switch foh-context here.

--- app/modules/auth/auth.server.ts
+++ app/modules/auth/auth.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/node';
-import { getSession } from './auth-session.server';
+import { redirectBack } from 'remix-utils/redirect-back';
 import { invariant } from '#app/utils/invariant';
 import { getUserByEmail } from '../api/foh/users';
 import { commitSession, getSession } from './auth-session.server';
@@ -13,7 +13,7 @@ export async function getUserId(request: Request) {
 export async function requireAnonymous(request: Request) {
 	const userId = await getUserId(request);
 	if (userId) {
-		throw redirect('/');
+		throw redirectBack(request, { fallback: '/' });
 	}
 }
 
