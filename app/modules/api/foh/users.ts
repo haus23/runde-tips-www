98d32c6867e4c839fd59e32bb044a92004194291
Micha Buchholz <micha@haus23.net>
2023-12-13 13:00:56 +0100

    refactor: Type the parsed roles string to an array of string enums.

--- app/modules/api/foh/users.ts
+++ app/modules/api/foh/users.ts
@@ -1,16 +1,20 @@
 import { db } from '../db.server';
+import { transformFromRawUser } from '../model/user';
 
 export async function getUserById(id: number) {
-	const user = await db.query.userTable.findFirst({
-		where: (user, { eq }) => eq(user.id, id),
-	});
-	return user ?? null;
+	const user = transformFromRawUser(
+		await db.query.userTable.findFirst({
+			where: (user, { eq }) => eq(user.id, id),
+		}),
+	);
+	return user;
 }
 
 export async function getUserByEmail(email: string) {
-	const user = await db.query.userTable.findFirst({
-		where: (user, { eq }) => eq(user.email, email),
-	});
-
-	return user ?? null;
+	const user = transformFromRawUser(
+		await db.query.userTable.findFirst({
+			where: (user, { eq }) => eq(user.email, email),
+		}),
+	);
+	return user;
 }
--- app/modules/api/model/user.ts
+++ app/modules/api/model/user.ts
@@ -1,6 +1,18 @@
 import { createSelectSchema } from 'drizzle-zod';
-import type { z } from 'zod';
+import { z } from 'zod';
 import { userTable } from '#db/schema';
 
-export const User = createSelectSchema(userTable);
+const Roles = z.array(z.enum(['PLAYER', 'ADMIN']));
+
+const RawUser = createSelectSchema(userTable);
+type RawUser = z.infer<typeof RawUser>;
+
+export const User = createSelectSchema(userTable).extend({ roles: Roles });
 export type User = z.infer<typeof User>;
+
+export function transformFromRawUser(rawUser?: RawUser) {
+	if (!rawUser) return null;
+
+	const user = { ...rawUser, roles: JSON.parse(rawUser.roles) };
+	return User.parse(user);
+}
